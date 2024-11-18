08d71fbf9980ab25d26e66ac9115c0ea9e47243b
Nigel-254 <nigeljasho@gmail.com>
2024-11-18 16:12:28 +0300

    Agriculture farm project

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from application import views as app_views
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', app_views.index, name='index'),
     path("application/", include('application.urls'))
 ]
